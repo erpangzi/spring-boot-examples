4eafdba8ca9ddc72c908fd536d41026125fef48e
rtwangpy <rtwangpy@yonyou.com>
2019-11-21 17:56:42 +0800

    update

---
+++ spring-boot-rabbitmq/src/main/java/com/rabbit/rabbit/hello/HelloSender.java
@@ -0,0 +1,35 @@
+package com.rabbit.rabbit.hello;
+
+
+
+import org.springframework.amqp.core.AmqpTemplate;
+import org.springframework.amqp.core.Message;
+import org.springframework.amqp.core.MessageProperties;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import java.util.Date;
+
+@Component
+public class HelloSender {
+
+	@Autowired
+	private AmqpTemplate rabbitTemplate;
+
+	public void send() {
+		String context = "hello " + new Date();
+		System.out.println("Sender : " + context);
+		this.rabbitTemplate.convertAndSend("hello", context);
+
+	}
+
+	public void send01(){
+		Message message = new Message("hello".getBytes(),new MessageProperties());
+		// 发送消息到默认的交换器，默认的路由键
+		rabbitTemplate.send(message);
+		// 发送消息到指定的交换器，指定的路由键
+		rabbitTemplate.send("direct.exchange","key.1",message);
+
+	}
+
+}
